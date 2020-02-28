7eed6a3fed27d1ec7c06849077647f8184ee0433
Parsabzh <parsabzh@gmail.com>
2020-02-29 00:41:15 +0330

    Add list to BookLlist Page

--- BookListRazor/Pages/BookList/BookListIndex.cshtml.cs
+++ BookListRazor/Pages/BookList/BookListIndex.cshtml.cs
@@ -7,22 +7,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookListRazor
+
+namespace BookListRazor.Pages.BookList
 {
-    public class IndexModel : PageModel
+    public class BookListIndexModel : PageModel
     {
-        private readonly ApplicationDbContext _db;
+        private ApplicationDbContext _db;
 
-        public IndexModel(ApplicationDbContext db)
-
+        public BookListIndexModel(ApplicationDbContext db)
         {
             _db = db;
+
         }
 
         public IEnumerable<Book> Books { get; set; }
         public async Task OnGet()
         {
             Books =await _db.Books.ToListAsync();
+           
         }
     }
 }
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -32,8 +32,8 @@ namespace BookListRazor
          
             services.AddDbContext<ApplicationDbContext>(options =>
                  options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddRazorPages().AddRazorRuntimeCompilation();
-           
+            services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
